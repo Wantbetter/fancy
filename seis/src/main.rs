3e1c3770e219e544c8d1afa3845f5e50eae5a5c0
halo <kkangwfu@163.com>
2019-02-28 10:11:00 +0800

    main change

--- seis/src/main.rs
+++ seis/src/main.rs
@@ -17,35 +17,17 @@ struct ForwardBridge {
     generate_prev: String, // 目标目录中需要进行正演模拟的文件夹的前缀
 }
 
-fn run_forward(exe_file: &str, config_file: &str) {
-    let m_path =
-    r"F:\毕设-研究生\地震波理论课相关软件\地震波理论模拟实习\model1";
-    let exe_file = r"F:\毕设-研究生\地震波理论课相关软件\地震波理论模拟实习\yzq-point.exe";
-    let config_file = r"F:\毕设-研究生\地震波理论课相关软件\地震波理论模拟实习\PARAMETER.txt";
-
-    let path = Path::new(m_path);
-    if !path.exists() {
-        fs::create_dir(path).expect("can't create dir");
-    }
-    println!("{}", path.to_str().unwrap());
-
-    let copy_exe_file = format!("{}\\yzq-point.exe", path.to_str().unwrap());
-    let copy_config_file = format!("{}\\PARAMETER.txt", path.to_str().unwrap());
-    fs::copy(exe_file, &copy_exe_file).expect("error in copy exe file");
-    fs::copy(config_file, &copy_config_file).expect("error in copy config file");
-    println!("{}", copy_exe_file);
-    //TODO: recover dir
-    env::set_current_dir(path).expect("error in set dir");
-    let mut exe = Command::new(copy_exe_file);
-    println!("{:?}", exe.status());
-}
-
 fn main() {
-    let forward = forward::ForwardBridge::new(
-        r"I:\dissertation\source\LUPENGmbNEW-PML.exe", r"I:\dissertation\source\PARAMETER.txt", 
-        r"I:\dissertation\models\三层水平均匀介质", "3-layers");
+    // let forward = forward::ForwardBridge::new(
+    //     r"I:\dissertation\source\LUPENGmbNEW-PML.exe", r"I:\dissertation\source\PARAMETER.txt", 
+    //     r"I:\dissertation\models\三层水平均匀介质", "3-layers");
+ 
+    // forward.model_ready();
 
-    forward.model_ready();
+    let mut template = forward::ForwardModelTemplate::default();
+
+    template.mod_prefix("graben");
+    template.write(r"F:\毕设-研究生\data\地堑");
 
     // let mut model_vp_grd = grd::Grd::from_grd_file(r"F:\毕设-研究生\data\三层水平均匀介质模型\3-layersvp.grd");
     // model_vp_grd.extract(r"F:\毕设-研究生\测试", "3-layersvp", 0, 48, 1, 2);
